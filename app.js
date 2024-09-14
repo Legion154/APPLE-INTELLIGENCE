8d22a320c2d005d5d8a8480184b92791a74baa08
Legion154 <darkdestiny700@gmail.com>
2024-09-14 13:25:31 +0500

    mobile version + site bar

---
+++ app.js
@@ -0,0 +1,11 @@
+function bars() {
+  document.getElementById("bar").style.width = "350px";
+  document.getElementById("bar").style.paddingLeft = "56px";
+  document.getElementById("bar").style.transition = "300ms"
+}
+
+function xmark() {
+  document.getElementById("bar").style.width = "0";
+  document.getElementById("bar").style.paddingLeft = "0";
+  document.getElementById("bar").style.transition = "300ms"
+}
